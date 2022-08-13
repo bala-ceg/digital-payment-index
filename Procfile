dff75dd7cec013a30172e5071c09a574b26a7c9b
Balaji Seetharaman <70808619+bala-ceg@users.noreply.github.com>
2022-08-13 23:28:39 +0530

    Committing Initial Files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python manage.py bower_install; python manage.py collectstatic; gunicorn digital_payment_index.wsgi
+
---
+++ load_daily_data.py
@@ -0,0 +1,18 @@
+import csv
+from digital_payment_index.models import  daily_index_data
+
+
+
+# with open('digital_payment_index/digital_index_past_value.csv') as csvfile:
+#       reader = csv.DictReader(csvfile)
+#       for row in reader:
+#             p = historic_index_data(date=row['\ufeffMonth'],index_value=row['Digital_Payment_Index'])
+#             p.save()
+
+
+
+with open('digital_payment_index/DPI_Daily_Movement.csv') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+             p =  daily_index_data(date=row['\ufeffDate'], UPI_Vol=row['UPI_VOL'], UPI_Val=row['UPI_VAL'],IMPS_Vol=row['IMPS_VOL'],IMPS_Val=row['IMPS_VAL'], NACH_Vol= row['NACH_VOL'] , NACH_Val=row['NACH_VAL'] ,NETC_Vol=row['NETC_VOL'],NETC_Val=row['NETC_VAL'],NEFT_Vol=row['NEFT_VOL'],NEFT_Val=row['NEFT_VAL'],RTGS_Vol=row['RTGS_VOL'],RTGS_Val=row['RTGS_VAL'],index_value=row['DPI'])
+             p.save()
