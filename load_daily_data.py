97256b71f880ff87b824404204092bf40c37f6cf
Balaji Seetharaman <70808619+bala-ceg@users.noreply.github.com>
2022-09-29 18:06:48 +0530

    Update load_daily_data.py
    Signed-off-by: Balaji Seetharaman <70808619+bala-ceg@users.noreply.github.com>

--- load_daily_data.py
+++ load_daily_data.py
@@ -17,7 +17,7 @@ from digital_payment_index.models import  daily_index_data
 
 
 
-with open('digital_payment_index/DPI_Daily_Movement.csv') as csvfile:
+with open('/app/digital_payment_index/DPI_Daily_Movement.csv') as csvfile:
       reader = csv.DictReader(csvfile)
       for row in reader:
              p =  daily_index_data(date=row['\ufeffDate'], UPI_Vol=row['UPI_VOL'], UPI_Val=row['UPI_VAL'],IMPS_Vol=row['IMPS_VOL'],IMPS_Val=row['IMPS_VAL'], NACH_Vol= row['NACH_VOL'] , NACH_Val=row['NACH_VAL'] ,NETC_Vol=row['NETC_VOL'],NETC_Val=row['NETC_VAL'],NEFT_Vol=row['NEFT_VOL'],NEFT_Val=row['NEFT_VAL'],RTGS_Vol=row['RTGS_VOL'],RTGS_Val=row['RTGS_VAL'],index_value=row['DPI'])
